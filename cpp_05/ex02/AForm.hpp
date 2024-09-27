55cccf090c8a1686f628974858aa4b4fac94be9a
belhadj haitam <steankir.haitam@gmail.com>
2024-09-27 11:27:42 +0100

    Aform is done

--- cpp_05/ex02/AForm.hpp
+++ cpp_05/ex02/AForm.hpp
@@ -6,7 +6,67 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 10:03:08 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/09/13 10:03:09 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/09/27 11:25:39 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef AFORM_HPP
+#define AFORM_HPP
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat;
+class AForm
+{
+    private:
+        const std::string name;
+        bool siGned;
+        unsigned int signGrade;
+        unsigned int execGrade;
+    public:
+        AForm();
+        AForm(const std::string& name, unsigned int signgrade, unsigned int execgrade);    
+        ~AForm();
+        
+
+        //exception
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw()
+                {
+                    return "AForm ERROR : Grade Too Low";
+                }
+            
+        };
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw()
+                {
+                    return "AForm ERROR : Grade Too High";
+                }
+        };
+
+        class NotSignedException : public std::exception
+        {
+            public:
+                virtual const char * what() const throw()
+                {
+                    return "AForm ERROR : NotSigned";
+                }
+        };
+        //Getters
+        std::string getName() const;
+        bool isSigned() const;
+        unsigned int getSigngrade() const;
+        unsigned int getExecGrade() const;
+        void beSigned(Bureaucrat &bureaucrat);
+        void ExecCheck(Bureaucrat const& executor) const;
+
+};
+std::ostream& operator<<(std::ostream& COUT, AForm const& other);
+
+#endif
--- cpp_05/ex02/Bureaucrat.hpp
+++ cpp_05/ex02/Bureaucrat.hpp
@@ -6,7 +6,52 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 10:03:17 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/09/13 10:03:18 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/09/27 11:26:47 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class AForm;
+
+
+class Bureaucrat
+{
+    private:
+        const std::string name;
+        unsigned int grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string& Name, int Grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+        //getters
+        const std::string getName() const;
+        unsigned int getGrade() const;
+        //public methods
+        void incrementGrade();
+        void decrementGrade();
+        void	signForm(AForm& obj);
+        //exceptions
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+            
+        };
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        
+};
+
+std::ostream& operator<<(std::ostream& COUT, Bureaucrat const &other);
+
+#endif
