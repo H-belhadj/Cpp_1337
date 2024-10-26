a84c11c35db6781eac46bcc7105dee4d822e4e6d
belhadj haitam <steankir.haitam@gmail.com>
2024-10-26 23:48:15 +0100

    first hand

--- cpp_06/ex01/Serializer.cpp
+++ cpp_06/ex01/Serializer.cpp
@@ -6,7 +6,31 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 22:27:50 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/10/26 22:27:51 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/10/26 23:46:21 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serializer.hpp"
+
+Serializer::Serializer(){}
+
+Serializer::~Serializer(){}
+
+Serializer::Serializer(const Serializer& obj)
+{
+    (void)obj;
+}
+Serializer& Serializer::operator=(const Serializer& obj)
+{
+    (void)obj;
+    return *this;
+}
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+Data* Serializer::deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data*>(raw);
+}
--- cpp_06/ex01/main.cpp
+++ cpp_06/ex01/main.cpp
@@ -6,7 +6,35 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 22:27:52 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/10/26 22:27:53 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/10/26 23:47:53 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Serializer.hpp"
+
+int main() {
+    // Create a Data object with some data
+    Data data = {"Example Name", 42, 84};
+
+    // Display original data
+    std::cout << "Original Data:\n";
+    std::cout << "Name: " << data._name << "\n";
+    std::cout << "num1: " << data.num1 << "\n";
+    std::cout << "num2: " << data.num2 << "\n\n";
+
+    // Serialize the pointer to `data`
+    uintptr_t raw = Serializer::serialize(&data);
+    std::cout << "Serialized pointer (integer representation): " << raw << "\n\n";
+
+    // Deserialize back to a `Data` pointer
+    Data* deserializedData = Serializer::deserialize(raw);
+
+    // Display deserialized data to confirm it's the same as the original
+    std::cout << "Deserialized Data:\n";
+    std::cout << "Name: " << deserializedData->_name << "\n";
+    std::cout << "num1: " << deserializedData->num1 << "\n";
+    std::cout << "num2: " << deserializedData->num2 << "\n";
+
+    return 0;
+}
