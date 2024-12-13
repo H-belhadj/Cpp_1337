3c81715b946eac3cc7b1e6a4eb06d1eb1264d919
belhadj haitam <steankir.haitam@gmail.com>
2024-12-13 23:45:20 +0100

    ex00 & ex01 is done

--- cpp_09/ex01/main.cpp
+++ cpp_09/ex01/main.cpp
@@ -1,12 +1,11 @@
 #include "RPN.hpp"
 
 int main(int ac, char **av) {
-    if (ac != 2) {
+    if (ac != 2)
+    {
         std::cerr << "Usage: " << av[0] << " \"[expression]\"" << std::endl;
         return (EXIT_FAILURE);
     }
-
-    RPN::calc(av[1]); // Use the correct method to calculate the expression
-
+    RPN::calc(av[1]);
     return (EXIT_SUCCESS);
 }
