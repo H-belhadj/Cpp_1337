82833767c88f64105160e203f666012453541d3a
belhadj haitam <steankir.haitam@gmail.com>
2024-12-15 01:08:27 +0100

    i think is finish

--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -1,13 +1,15 @@
 #include "PmergeMe.hpp"
 #include <iostream>
-int main(int argc, char **argv) {
-    if (argc < 3) {
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+	{
         std::cout << "Usage: ./PmergeMe [positive integers]" << std::endl;
         return 1;
     }
 
-    PmergeMe pmm;
-    pmm.start(argv, argc);
+    PmergeMe sakan;
+    sakan.start(argv, argc);
 
     return 0;
 }
